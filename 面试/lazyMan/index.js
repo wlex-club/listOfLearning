0428be6915b5ba93a394f51c14b757d4ab4073f0
vle <weile.wang@realai.ai>
2020-11-03 19:20:12 +0800

    add files

--- 面试/lazyMan/index.js
+++ 面试/lazyMan/index.js
@@ -3,7 +3,7 @@
  * LazyMan("Hank")输出:
  * Hi! This is Hank!
   
- * azyMan("Hank").sleep(10).eat("dinner")输出
+ * LazyMan("Hank").sleep(10).eat("dinner")输出
  * Hi! This is Hank!
  * //等待10秒..
  * Wake up after 10
@@ -20,10 +20,58 @@
  * Hi This is Hank!
  * Eat supper
  * **/
-var taskList = []
-
-function LazyMan() {}
-LazyMan.prototype.eat = function (str) {
-    return this
+function Test(name) {
+  this.task = [];
+  let fn = () => {
+    console.log(name);
+    this.next();
+  };
+  this.task.push(fn);
+  setTimeout(() => {
+    this.next();
+  }, 0);
+  return this;
 }
 
+Test.prototype.firstSleep = function(timer) {
+  console.time('firstSleep');
+  let that = this;
+  let fn = () => {
+    setTimeout(() => {
+      console.timeEnd('firstSleep');
+      that.next();
+    }, timer * 1000);
+  };
+  this.task.unshift(fn);
+  return this;
+};
+
+Test.prototype.sleep = function(timer) {
+  console.time('sleep');
+  let that = this;
+  let fn = () => {
+    setTimeout(() => {
+      console.timeEnd('sleep');
+      that.next();
+    }, timer * 1000);
+  };
+  this.task.push(fn);
+  return this;
+};
+
+Test.prototype.eat = function(dinner) {
+  let that = this;
+  let fn = () => {
+    console.log(dinner);
+    that.next();
+  };
+  this.task.push(fn);
+  return this;
+};
+
+Test.prototype.next = function(dinner) {
+  let fn = this.task.shift();
+  fn && fn();
+};
+
+new Test('test').firstSleep(3).sleep(5).eat('dinner');
